3d33e2cd3b088004c8983cb990e59dbe597b25d7
dongkeon, oh <ho2921ho@naver.com>
2019-05-18 11:36:37 +0900

    1

--- Preprocessing_Demian.py
+++ Preprocessing_Demian.py
@@ -3,6 +3,8 @@ import os
 import re
 from datetime import datetime
 from tqdm import tqdm
+import pickle
+
 
 os.chdir(r'C:\DATA\L.point2019\data')
 os.listdir()
@@ -37,20 +39,24 @@ Session['MONTH'] = list(map(lambda x:x[5:7],Session['SESS_DT']))
 Session['WEEK'] = list(map(lambda x:datetime.strptime(x, '%Y-%m-%d').strftime('%Y-%U-1'),Session['SESS_DT'])) 
 Session['WEEK'] = list(map(lambda x:datetime.strptime(x, '%Y-%U-%w').strftime('%Y-%m-%d'),Session['WEEK'])) 
 
-## 0 = 월요일, 6 = 일요일
+## 0 = 월요일, 6 = 일요일 # 1:33
+start = datetime.now()
 Session['DAY'] = list(map(lambda x:datetime.strptime(x, '%Y-%m-%d').weekday(),Session['SESS_DT'])) 
+end = datetime.now()
+print (end - start)    
 
-## 휴일 변수;REST 추가
+## 휴일 변수;REST 추가 #  2:29
 hoilday = ['2018-05-05', '2018-05-22', '2018-06-06', '2018-08-15', '2018-09-23', '2018-09-24', '2018-09-25']
 
 rest = []
 for idx, date in tqdm(enumerate(Session.SESS_DT)):
-    if date in hoilday or Session.DAY[idx] == 5 or Session.DAY[idx] == 6:
+    if  Session.DAY[idx] >= 5 or date in hoilday: # or 논리연산자 구성할 때, True 빈도가 더 많은 것을 앞으로! 
         rest.append(1)
     else:
         rest.append(0)
 
 Session['REST'] = rest
+
 # Search1, Search 2 전처리
 # merge를 위해 SESS_DT 형식 동일하게 변경. 
 Search2['SESS_DT'] = Search2['SESS_DT'].astype(str) # int object is not subsriptable 
@@ -62,26 +68,28 @@ Search2['SEARCH_CNT'] = list(map(lambda x:x.replace(",",""), Search2['SEARCH_CNT
 Search2['SEARCH_CNT'] =  Search2['SEARCH_CNT'].astype(int)
 Search2.rename(columns={'SEARCH_CNT': 'SEARCH_TOT'}, inplace=True)
 
-# 전체 검색량 대비 개인 검색량  변수 생성.
+# 전체검색량, 검색 키워드 갯수, 개인검색량, 전체검색량 대비 개인 검색량, 변수 생성.
 Search = pd.merge(Search1,Session.loc[:,['CLNT_ID','SESS_ID','SESS_DT']],how = 'left', on = ['CLNT_ID','SESS_ID']) 
 Search = pd.merge(Search,Search2.loc[:,['SESS_DT','KWD_NM','SEARCH_TOT']],how = 'left', on = ['KWD_NM','SESS_DT']) 
-Search['SEARCH_RATIO'] = Search.SEARCH_CNT / Search.SEARCH_TOT
+cnt = Search.groupby(['CLNT_ID','SESS_ID']).count()['KWD_NM'] # 순서 유의.
+Search = Search.groupby(['CLNT_ID','SESS_ID']).sum() # 이 부분에서 고유한 키값으로 줄어듬. 
+Search['KWD_CNT'] = cnt
+Search['SEARCH_RATIO'] = Search.SEARCH_CNT / Search.SEARCH_TOT  
 
+with open('Search.pickle','wb') as f:
+    pickle.dump(Search,f)
+    
 # merge
 raw = pd.merge(Pruduct,Custom, how = 'left', on = ['CLNT_ID']) 
 raw = pd.merge(raw,Master, how = 'left', on = ['PD_C']) 
 raw = pd.merge(raw,Session, how = 'left', on = ['CLNT_ID','SESS_ID']) 
+raw = pd.merge(raw,Search,how = 'left', on = ['CLNT_ID','SESS_ID']) 
 
-Pruduct.head()
-# 한 세션에서 총 검색량 추가
-raw = pd.merge(raw,Search1.groupby(['CLNT_ID','SESS_ID']).sum(), how = 'left', on = ['CLNT_ID','SESS_ID']) 
-# 한 세션에서 검색 종류 추가
-raw = pd.merge(raw,Search1.groupby(['CLNT_ID','SESS_ID']).count()['KWD_NM'], how = 'left', on = ['CLNT_ID','SESS_ID']) 
-# 열 이름 변경 
-raw.rename(columns={'SEARCH_CNT': 'SEARCH_TOT_CNT', 'KWD_NM': 'KWD_NUM'}, inplace=True)
-# 전체 검색량 대비 개인 검색량  변수 추가.
-raw = pd.merge(raw,Search.groupby(['CLNT_ID','SESS_ID']).mean()['SEARCH_RATIO'], how = 'left', on = ['CLNT_ID','SESS_ID']) 
 
-list(raw)
-raw.CLAC3_NM
-test = raw[raw.CLAC3_NM == '스킨케어세트']
+with open('raw.pickle', 'wb') as f:
+    pickle.dump(raw, f)
+
+
+with open('Search.pickle','rb') as f:
+    data = pickle.load(f)
+
